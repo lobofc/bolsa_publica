f29dfd9d55760347514e25b1b467d2c3e8d82fd1
ee <lobofc10@gmail.com>
2018-04-09 21:47:33 -0400

    depuracion de callback

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,6 +1,6 @@
 class ContractsController < ApplicationController
  before_action :set_contract, only: [:show, :edit, :update, :destroy]
- before_action :set_setting, only: [:index, :show, :edit, :update, :destroy, :new, :concurso]
+ before_action :set_setting, only: [:index, :show, :edit, :update, :destroy, :new, :contrataciones]
   # GET /contracts
   # GET /contracts.json
   def index
